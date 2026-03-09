e2dca58aa89a9f517e0e2e9bff37ef353507ef49
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2026-03-09 19:07:23 +0700

    fix(organiclever-be): correct HelloController package declaration after move

--- apps/organiclever-be/src/main/java/com/organiclever/be/hello/controller/HelloController.java
+++ apps/organiclever-be/src/main/java/com/organiclever/be/hello/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.organiclever.be.controller;
+package com.organiclever.be.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
